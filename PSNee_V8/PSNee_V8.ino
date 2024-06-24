8af87d48fc934ab9eed7db15ef148ec23b3ef003
kalymos <schumachernicolas@gmail.com>
2024-06-24 20:19:35 +0200

    Update PSNee_V8.ino

--- PSNee_V8/PSNee_V8.ino
+++ PSNee_V8/PSNee_V8.ino
@@ -24,7 +24,7 @@
 //   If a BIOS checksum is specified, it is more important than the SCPH model number!
 //------------------------------------------------------------------------------------------------	
 
-//#define SCPH_xxx1        // Use for all NTSC-U/C models. No BIOS patching needed. 
+//#define SCPH_XXX1        // Use for all NTSC-U/C models. No BIOS patching needed. 
 //#define SCPH_XXX2        // Use for all PAL FAT models. No BIOS patching needed. 
 //#define SCPH_103         // No BIOS patching needed.
 //#define SCPH_102         // DX - D0, AX - A7. BIOS ver. 4.4e, CRC 0BAD7EA9 | 4.5e, CRC 76B880E5
