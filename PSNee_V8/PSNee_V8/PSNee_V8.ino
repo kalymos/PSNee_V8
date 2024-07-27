b92a7a87abd88d21b6400b2b7170e0e19943c87b
kalymnos <schumachernicolas@gmail.com>
2024-07-27 19:27:17 +0200

    add

--- PSNee_V8/PSNee_V8/PSNee_V8.ino
+++ PSNee_V8/PSNee_V8/PSNee_V8.ino
@@ -8,8 +8,8 @@
 //                         Select your chip
 //------------------------------------------------------------------------------------------------
 
-#define ATmega328_168  // Fuses: JAP_FAT - H: DF, L: EE, E: FF; Other - H: DF, L: FF, E: FF.
-//#define ATmega32U4_16U4
+//#define ATmega328_168  // Fuses: JAP_FAT - H: DF, L: EE, E: FF; Other - H: DF, L: FF, E: FF.
+#define ATmega32U4_16U4
 //#define ATtiny85_45_25
 /*  PinVCC-3.5v, PinGND-GND, 
     Pin2-BIOS AX, Pin3-BIOS AY, 
@@ -28,11 +28,11 @@
 //------------------------------------------------------------------------------------------------
 
 //#define SCPH_xxx1        // Use for all NTSC-U/C models. No BIOS patching needed.
-#define SCPH_xxx2        // Use for all PAL FAT models. No BIOS patching needed.
+//#define SCPH_xxx2        // Use for all PAL FAT models. No BIOS patching needed.
 //#define SCPH_103         // No BIOS patching needed.
 //#define SCPH_102         // DX - D0, AX - A7. BIOS ver. 4.4e, CRC 0BAD7EA9 | 4.5e, CRC 76B880E5
 //#define SCPH_100         // DX - D0, AX - A7. BIOS ver. 4.3j, CRC F2AF798B
-//#define SCPH_7000_9000  // DX - D0, AX - A7. BIOS ver. 4.0j, CRC EC541CD0
+#define SCPH_7000_9000  // DX - D0, AX - A7. BIOS ver. 4.0j, CRC EC541CD0
 //#define SCPH_5500        // DX - D0, AX - A5. BIOS ver. 3.0j, CRC FF3EEB8C
 //#define SCPH_3500_5000   // DX - D0, for 40-pin BIOS: AX - A4, for 32-pin BIOS: AX - A5. BIOS ver. 2.2j, CRC 24FC7E17 | 2.1j, CRC BC190209
 //#define SCPH_3000        // DX - D5, for 40-pin BIOS: AX - A6, AY - A7, for 32-pin BIOS: AX - A7, AY - A8. BIOS ver. 1.1j, CRC 3539DEF6
